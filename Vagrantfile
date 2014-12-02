2b7b9ef53dc565bb3f1da9e6662cfaf0b2032dea
Maxi Padulo <maxi.padulo@gmail.com>
2014-12-02 17:30:14 +0100

    fixes #480 @3h30m

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,9 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
-CONFIG_FILE = "vagrant.rb"
+#
+# Requires an external config file 
+CONFIG_FILE_PROD= "../vagrant-prod.rb"
+CONFIG_FILE_DEV = "../vagrant-dev.rb"
 
 # Default configs (override in config file only)
 PRODUCTION_MODE=true
@@ -13,29 +16,36 @@ MAIN_RAM=1024
 EXTRA_MACHINES=0
 EXTRA_MACHINES_RAM=756
 EXTRA_MACHINES_BASE_IP="192.168.55.5" # Last digit filled by the counter
+#PROJECT_NAME="unnamedApp" # Should be overrided un CONFIG_FILE! Should be
+#a slug
 
-if File.exist?(CONFIG_FILE)
-    puts "INFO: Loading local settings from: #{CONFIG_FILE}"
-    require CONFIG_FILE
-    include CustomConf
+if File.exist?(CONFIG_FILE_DEV)
+    puts "DEVELOPMENT: Loading settings from: #{CONFIG_FILE_DEV}"
+    require_relative CONFIG_FILE_DEV
 else
-    puts "INFO: Production settings"
+    puts "PRODUCTION: Loading setting from (#{CONFIG_FILE_PROD})"
+    require_relative CONFIG_FILE_PROD
 end
+puts "\nPRODUCTION_MODE: #{PRODUCTION_MODE}"
+puts "USE_GUI: #{USE_GUI}"
 
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.provider :virtualbox do |vb| 
-        vb.gui = false 
+        vb.gui = USE_GUI 
         # Use VBoxManage to customize the VM
         vb.customize ["modifyvm", :id, "--memory", EXTRA_MACHINES_RAM]
     end
+
+    config.vm.synced_folder "../", "/repo"
 
     config.vm.define "main", primary: true do |main|
         main.vm.box = "ubuntu/trusty64"
         main.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
         config.vm.provider :virtualbox do |vb| 
+            vb.name = PROJECT_NAME
             vb.customize ["modifyvm", :id, "--memory", MAIN_RAM]
             vb.customize ["modifyvm", :id, "--ioapic", "on"]
             vb.customize ["modifyvm", :id, "--cpus", MAIN_CPU_COUNT]
@@ -57,12 +67,14 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
             main.vm.network :forwarded_port, guest: 80, host: 8080
         end
 
-        main.vm.provision :shell, :path => "files/puppet-install.sh"
-        main.vm.provision :shell, :path => "files/puppet-modules.sh"
+        main.vm.provision :shell, :path => "puppet-install.sh"
+        main.vm.provision :shell, :path => "puppet-modules.sh"
 
         main.vm.provision :puppet do |puppet|
             puppet.manifests_path = "manifests"
             puppet.manifest_file  = "main.pp"
+            puppet.facter  = {"projectid" => PROJECT_NAME}
+            puppet.options  = "--verbose"
         end
     end 
 
--- manifests/main.pp
+++ manifests/main.pp
@@ -3,14 +3,21 @@ Exec { path => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin' }
 # Global variables
 $inc_file_path = '/vagrant/files' # Absolute path to the files directory 
 
-$user = 'user' # User to create
+$user = 'admin' # User to create
 $password = 'abcdef1' # The user's password
 
-$project = 'vtfx' # Used in nginx and uwsgi
-$domain_name = 'vtfx.mainstorconcept.de' # Used in nginx, uwsgi and virtualenv directory
-$db_name = 'vtfx' # Mysql database name to create
-$db_user = 'vtfx' # Mysql username to create
-$db_password = 'vtfx' # Mysql password for $db_user
+
+if $projectid == undef {
+    fail("Sorry man, you suck: 'projectid fact not defined'")
+}
+
+$project = "${projectid}"  # Used in nginx and uwsgi
+$domain_name = "${project}.mainstorconcept.de" # Used in nginx, uwsgi and virtualenv directory
+$db_name = "${project}" # Mysql database name to create
+$db_user = "${project}" # Mysql username to create
+$db_password = "${project}" # Mysql password for $db_user
+
+$alias_run_puppet="alias pp='sudo FACTER_PROJECTID=${project} puppet apply /vagrant/manifests/main.pp'"
 
 $tz = 'Europe/Berlin' # Timezone
 
@@ -37,6 +44,7 @@ class { 'python':
 class { 'apt':
   always_apt_update    => true,
 }
+
 Class['apt'] -> Class['python']      
 Class['apt'] -> Class['paquetes']      
 Class['apt'] -> Class['database']      
@@ -91,40 +99,39 @@ class users {
         mode => '0600',
         source =>"${inc_file_path}/ssh/id_rsa",
     }
-
     file { "/home/$user/.bash_aliases":
         ensure => "file",
         owner  => "$user",
         group  => "$user",
         content  => "alias wd='cd /var/www/$project/repo; source /var/www/$project/env/bin/activate'
                    \nalias run='/var/www/$project/repo/webapp/manage.py runserver 0.0.0.0:8888'
-                   \nalias pp='sudo puppet apply /vagrant/manifests/replicator.pp'",
+                   \n${alias_run_puppet}",
         mode   => 755,
     }
     file { "/home/vagrant/.bash_aliases":
         ensure => "file",
         owner  => "vagrant",
         group  => "vagrant",
-        content  => "alias pp='sudo puppet apply /vagrant/manifests/site.pp'",
+        content  => "${alias_run_puppet}",
         mode   => 755,
     }
 }
 
 class virtualenv {
-    python::virtualenv { "/var/www/${project}/env":
-    ensure       => present,
-    version      => 'system',
-    requirements => "/var/www/${project}/repo/webapp/requirements.txt",
-    #proxy        => 'http://proxy.domain.com:3128',
-    #systempkgs   => true,
-    distribute   => false,
-    owner        => "$user",
-    group        => "$user",
-    #cwd          => '/var/www/virtualenvs/${project}',
-    timeout      => 100,
-    require => [Class['app_sources'], Class['database']],
-    before => Class['app_deploy'],
-    }
+    #python::virtualenv { "/var/www/${project}/env":
+    #ensure       => present,
+    #version      => 'system',
+    #requirements => "/var/www/${project}/repo/webapp/requirements.txt",
+    ##proxy        => 'http://proxy.domain.com:3128',
+    ##systempkgs   => true,
+    #distribute   => false,
+    #owner        => "$user",
+    #group        => "$user",
+    ##cwd          => '/var/www/virtualenvs/${project}',
+    #timeout      => 100,
+    #require => [Class['app_sources'], Class['database']],
+    #before => Class['app_deploy'],
+    #}
 }
 
 class paquetes {
@@ -133,7 +140,7 @@ class paquetes {
     package { $vcs: ensure => latest }
 
     package { 'fabric':
-        ensure => latest,
+        ensure => installed,
         provider => pip,
         require => Package['python-pip']
     }
@@ -143,10 +150,8 @@ class app_sources {
     $dirs = [ "/var/www", 
               "/var/www/${project}",
               "/var/www/${project}/media", 
-              "/var/www/${project}/static", 
 #              "/var/www/${project}/env",
-]
-
+] 
     file { $dirs:
         ensure => "directory",
         owner  => "$user",
@@ -155,6 +160,7 @@ class app_sources {
     }
 
     file { "/var/www/${project}/static": 
+        ensure => "directory",
         mode => 777,
     }
 
@@ -178,13 +184,13 @@ class app_sources {
 }
 
 class app_deploy {
-    exec { "fab deploy:host=vagrant@localhost --password=vagrant":
-        #cwd     => "/var/www/${project}/repo/webapp",
-        cwd     => "/vtfx",
-        provider=> shell,
-        logoutput => true,
-        #path    => ["/usr/bin", "/usr/sbin"]
-    }
+    #exec { "fab deploy:host=vagrant@localhost --password=vagrant":
+        ##cwd     => "/var/www/${project}/repo/webapp",
+        #cwd     => "/vtfx",
+        #provider=> shell,
+        #logoutput => true,
+        ##path    => ["/usr/bin", "/usr/sbin"]
+    #}
 
 }
 
@@ -213,17 +219,18 @@ class uwsgi {
         ensure => installed,
         require => Class['paquetes'],
     }
-    file { '/etc/uwsgi/apps-available/vtfx.ini':
+    file { "/etc/uwsgi/apps-available/${project}.ini":
         ensure => present,
         owner => 'root',
         group => 'root',
         mode => '0644',
-        source =>"${inc_file_path}/uwsgi/vtfx.ini",
+        content => template("${inc_file_path}/uwsgi/main.erb"),
+        #source =>"${inc_file_path}/uwsgi/main.ini",
         require => Package['uwsgi'],
     }
-    file { '/etc/uwsgi/apps-enabled/vtfx.ini':
+    file { "/etc/uwsgi/apps-enabled/${project}.ini":
         ensure => link,
-        target => '/etc/uwsgi/apps-available/vtfx.ini',
+        target => "/etc/uwsgi/apps-available/${project}.ini",
         require => Package['uwsgi'],
     } 
     service { 'uwsgi':
@@ -232,8 +239,8 @@ class uwsgi {
         enable => true,
         hasrestart => false,
         hasstatus => false,
-        require => [ File['/etc/uwsgi/apps-enabled/vtfx.ini'], Class['virtualenv'] ],
-        subscribe => File['/etc/uwsgi/apps-available/vtfx.ini'],
+        require => [ File["/etc/uwsgi/apps-enabled/${project}.ini"], Class['virtualenv'] ],
+        subscribe => File["/etc/uwsgi/apps-available/${project}.ini"],
     }
 }
 
@@ -242,23 +249,23 @@ class nginx {
         ensure => present,
         require => Class['paquetes'],
     }
-    file { '/etc/nginx/sites-available/vtfx':
+    file { "/etc/nginx/sites-available/${project}":
         ensure => file,
         owner => 'root',
         group => 'root',
         mode => '640',
-        source =>"${inc_file_path}/nginx/vtfx",
-        #content => template("${inc_file_path}/nginx/vtfx.erb"),
+        #source =>"${inc_file_path}/nginx/${project}",
+        content => template("${inc_file_path}/nginx/main.erb"),
         require => Package['nginx'],
     }
     # Disable default config
-    file { '/etc/nginx/sites-enabled/default':
+    file { "/etc/nginx/sites-enabled/default":
         ensure => absent,
         require => Package['nginx'],
     } 
-    file { '/etc/nginx/sites-enabled/vtfx':
+    file { "/etc/nginx/sites-enabled/${project}":
         ensure => link,
-        target => '/etc/nginx/sites-available/vtfx',
+        target => "/etc/nginx/sites-available/${project}",
         require => Package['nginx'],
     } 
     service { 'nginx':
@@ -266,7 +273,7 @@ class nginx {
         enable => true,
         hasstatus => true,
         hasrestart => true,
-        subscribe => File['/etc/nginx/sites-available/vtfx'],
+        subscribe => File["/etc/nginx/sites-available/${project}"],
     }
 }
 
