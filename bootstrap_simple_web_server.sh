193457ea82ebebe3c6eed2a866316723f01b9f10
Maxi Padulo <maxi.padulo@gmail.com>
2014-12-16 16:40:02 +0100

    refs #493 @20m

--- bootstrap_simple_web_server.sh
+++ bootstrap_simple_web_server.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+WWW_USER=vagrant
 
 apt-get update
 apt-get install -y apache2
-#if ! [ -L /var/www ]; then
+if ! [ -L /var/www ]; then
+  chown -R vagrant /var/www
   #rm -rf /var/www
   #ln -fs /vagrant /var/www
-#fi
+fi
